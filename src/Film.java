45d49f1fd6c483e7a8360c224821a576d4815b4c
Lannuzel <tristan.lannuzel@hotmail.fr>
2019-11-19 16:25:48 +0100

    Avancement tests

--- src/Film.java
+++ src/Film.java
@@ -1,4 +1,3 @@
-
 import java.awt.Image;
 import java.util.ArrayList;
 import java.util.Date;
--- src/Tests.java
+++ src/Tests.java
@@ -1,40 +1,108 @@
-public class Tests {
-	//Test 1 : Louer un DVD
-	static Controleur controleur;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Tests {
 	
 	private static void etatCompte(Client c) {
 		String nature = (c instanceof ClientVIP) ? "VIP" : "CB";;
 		System.out.println("Nature du compte : " + nature);
-		System.out.println("Nombre de location actuel :" );
+		System.out.println("Location actuel :" );
+		for(int i=0; i < c.getLocs().size(); i++) {
+			System.out.println("Location n°"+i+" : "+c.getLocs().get(i));
+		}
 	}
-	
-	private static void testLouer(Client c) { //Test de location
+	protected static void testLouer(Client c) { //Test de location
 		etatCompte(c);
-		System.out.println("Location...");
-		//traitement...
+		Dvd aLouer;
+		System.out.println("Location DVD aléatoire...");
+//		DVD aLouer = DVDDISPO.getRandom()
+		c.louer(aLouer);
 		etatCompte(c);
 	}
 
 	private static void testRendre(Client c) {
+		Scanner sc = new Scanner(System.in);
 		etatCompte(c);
+		System.out.println("Index du DVD à rendre ?");
+		int aRendre = sc.nextInt();
 		System.out.println("Rendu...");
-		//traitement...
+		c.suppLoc(c.getLocs().get(aRendre));
 		etatCompte(c);
 	}
 
-	private static void testLouerMultiple(Client c, int nbLocation) {
+	private static void testLocationMultiple(Client c, int nbLocation) {
 		etatCompte(c);
-		System.out.println("Locations...");
-		//traitement...
+		Dvd aLouer;
+		System.out.println("Locations de "+nbLocation+" DVDs...");
+		for (int i = 0; i < nbLocation; i++) {
+//			DVD aLouer = DVDDISPO.getRandom()
+			c.louer(aLouer);
+		}
 		etatCompte(c);
 	}
 
 	public static void main(String[] args) {
-		controleur = new Controleur();
-		Client clientCB = new ClientCB("CB", "Prenom", "nom.prenom@mail.com", "0123456789", 01, "1111222233334444");
-		Client clientVIP = new ClientVIP("VIP", "Prenom", "nom.prenom@mail.com", "0123456789", 01, "1111222233334444");
+		ArrayList<Client> clients = new ArrayList<Client>();
+		Client clientTest;
+		String choixTest;
+		Scanner sc = new Scanner(System.in);
 
-		testLouer(clientVIP);
+		clients.add(new ClientCB("CB", "Prenom", "nom.prenom@mail.com", "0123456789","1111222233334444"));
+		clients.add(new ClientVIP("VIP", "Prenom", "nom.prenom@mail.com", "0123456789","1111222233334444"));
+
+		while(true) {
+			do {
+				System.out.println("Type de client à tester ?");
+				System.out.println("0 : CB");
+				System.out.println("1 : VIP");
+				int choix = sc.nextInt();
+				System.out.println(choix);
+				if(choix !=0 && choix!=1) {
+					System.out.println("Choix  invalide");
+					clientTest=null;
+				}else {
+					clientTest = clients.get(choix);
+				}
+			}while(clientTest==null);
+
+			do {
+				System.out.println("Choix de test :");
+				System.out.println("l : Test de location");
+				System.out.println("r : Test de rendu");
+				System.out.println("ml : Test de location multiple");
+				System.out.println("q : Quitter programme test ou changer de type de client");
+				choixTest = sc.next();
+				
+				switch (choixTest) {
+				case "l":
+					testLouer(clientTest);
+					break;
+
+				case "r":
+					testRendre(clientTest);
+					break;
+
+				case "ml":
+					System.out.println("Nombre de location à effectuer ? Nombre de location actuelle du client : "+ clientTest.getLocs().size());
+					int nbLocation = sc.nextInt();
+					if(nbLocation<0) {
+						System.out.println("Entrer un nombre de location positif");
+						break;
+					}
+					testLocationMultiple(clientTest, nbLocation);
+					break;
+
+				case "q":
+					System.out.println("Fin de test");
+					break;
+
+				default:
+					System.out.println("Choix invalide");
+				}
+			}while(!choixTest.equals("q"));
+		}
 	}
 }
+
+
+
